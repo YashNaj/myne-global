26fe0ac782a63e10dc727cee48771f2f9b18f9ee
YashNaj <yashalnajeeb@gmail.com>
2023-01-19 21:05:07 -0800

    commit

--- src/routes/api/send-mail/+server.ts
+++ src/routes/api/send-mail/+server.ts
@@ -4,7 +4,7 @@ import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(VITE_SENDGRID_API_KEY);
 
 export const POST: RequestHandler = async () => {
-  let data = await.req.body();
+  let data = await req.body;
   console.log(data);
   await sgMail.send(data).then(console.log("success")).catch(error);
   return new Response("success", {staus: 200})
