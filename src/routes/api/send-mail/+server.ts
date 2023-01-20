da52bab245ff399b357d2c09fcb9155dbaff6115
YashNaj <yashalnajeeb@gmail.com>
2023-01-19 21:03:07 -0800

    commital

--- src/routes/api/send-mail/+server.ts
+++ src/routes/api/send-mail/+server.ts
@@ -1,7 +1,7 @@
 import { json, type RequestHandler, error } from "@sveltejs/kit";
 import { SENDGRID_API_KEY } from "$env/static/private";
 import sgMail from "@sendgrid/mail";
-sgMail.setApiKey(SENDGRID_API_KEY);
+sgMail.setApiKey(VITE_SENDGRID_API_KEY);
 
 export const POST: RequestHandler = async () => {
   let data = await.req.body();
