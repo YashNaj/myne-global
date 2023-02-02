609610cdf16ff637922fd739c0a600445a637bce
YashNaj <yashalnajeeb@gmail.com>
2023-02-01 23:52:23 -0800

    please work

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,11 +5,3 @@ import {VITE_MONGO_URI} from '$env/static/private';
 import * as db from '$lib/mongoose'
 import { supabase } from './lib/supabase';
 export const handle = handleHooks(auth);
-try{ 
-    await mongoose.connect(VITE_MONGO_URI)
-    console.log("Starting MongoDB")
-}catch(e){
-    console.error(e)
-}finally{
-    console.log("Successfully connected to MongoDB")
-}
