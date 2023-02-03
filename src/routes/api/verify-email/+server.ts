7cb912cbd1284985aa4899cada529c3882b2483e
YashNaj <yashalnajeeb@gmail.com>
2023-02-02 22:48:09 -0800

    assss

--- src/routes/api/verify-email/+server.ts
+++ src/routes/api/verify-email/+server.ts
@@ -1,16 +1,17 @@
+
 import { auth } from "$lib/auth/lucia";
 import { Parsers } from "$lib/schema/parsers";
 import { error, redirect, type RequestHandler } from "@sveltejs/kit";
 import { z } from "zod";
 import { prisma } from "$lib/prisma";
 export const GET: RequestHandler = async ({ url, user }) => {
-  const { token } = Parsers.params(url, z.object({ token: z.string() }));
+  const  {token}  = Parsers.params(url, z.object({ token: z.string() }));
   console.log(token);
   try {
     const request = await prisma.emailVerificationRequest.findUnique({
       where: {
-        token: token,
-      },
+        token: token
+      }
     });
 
     console.log(request);
@@ -25,9 +26,10 @@ export const GET: RequestHandler = async ({ url, user }) => {
       valid: true,
     });
 
-    throw redirect(302, "/");
   } catch (error) {
     console.log(error);
+  }finally{
+    return new Response({"message": "success!"})
+    throw redirect(302, "/");
   }
-  return {'message': "success"}
 };
