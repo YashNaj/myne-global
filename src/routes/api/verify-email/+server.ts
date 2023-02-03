7c8914aaefe0773fab1920a07eb07e7b24589a03
YashNaj <yashalnajeeb@gmail.com>
2023-02-02 22:00:26 -0800

    there is no reason for there to be this problem

--- src/routes/api/verify-email/+server.ts
+++ src/routes/api/verify-email/+server.ts
@@ -4,30 +4,37 @@ import { error, redirect, type RequestHandler } from "@sveltejs/kit";
 import { z } from "zod";
 import {prisma}  from "$lib/prisma";
 export const GET: RequestHandler = async ({ url }) => {
-const expression = "https://myneglobal.com/api/verify-email?token=" || "http://localhost:5173/api/verify-email?token=";
   const {token}  = Parsers.params(url, z.object({ token: z.string() }));
-  const verificationRequest = await prisma.EmailVerificationRequest.findUnique(
-    {
-      where: {
-        token: token,
-      },
-    }
-  );
-  console.log(verificationRequest);
-  if (!verificationRequest) throw error(400, "Invalid token");
-  const { userId, expiresAt } = verificationRequest;
+  console.log(token)
+  try{
+    const request = await prisma.emailVerificationRequest.findUnique({
+        where: {
+          token: token, 
+        },
+        select: {
+       
+            emailVerificationRequest: true, 
+        },
+      })
+      
+    console.log(request)
+    if (!request) throw error(400, "Invalid token");
+    const { userId, expiresAt } = request;
+  
+    if (request.expiresAt < new Date()) throw error(400, "Token expired");
+  
+    const user = await auth.getUser(userId);
+    if (!user) throw error(400, "Invalid token");
+    await auth.updateUserAttributes(user, {
+        valid: true,
+      });
+    
+      throw redirect(302, "/");
+      return Response.send(JSON.stringify(token));
+  }
+  
+  catch (error){
+    console.log(error);
+  }
 
-  if (expiresAt < new Date()) throw error(400, "Token expired");
-
-  const user = await auth.getUser(userId);
-  if (!user) throw error(400, "Invalid token");
-
-  await auth.updateUserAttributes(user, {
-    valid: true,
-  });
-
-  await verificationRequest.remove();
-
-  throw redirect(302, "/");
-  return Response.send(JSON.stringify(token));
 };
