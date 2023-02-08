9e1992d200d79aada1e1714eed73723248144c44
YashNaj <yashalnajeeb@gmail.com>
2023-02-08 13:15:25 -0800

    added validation to get-records endpoint

--- src/routes/api/get-brands/+server.ts
+++ src/routes/api/get-brands/+server.ts
@@ -2,13 +2,17 @@ import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 import {Prisma, PrismaClient} from '@prisma/client';
 export const GET: RequestHandler = async () => {
+    const {user, session} = await locals.validateSessionUser(); 
+    if (!session){
+        return (404, 'NO PRIVELAGES')
+    }
     const prisma = new PrismaClient();
     const brands  = await prisma.brand.findMany({
         select: {
             jewlery:true, 
         }
     }); 
-    
+
     
     return json(brands);
 };
