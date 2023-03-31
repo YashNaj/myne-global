10029e6dd94b1ac1803f194c44991e3c9a3c7e07
YashNaj <yashalnajeeb@gmail.com>
2023-03-31 03:40:22 -0700

    vercel font support

--- src/lib/server/pdf/generateCertificate.ts
+++ src/lib/server/pdf/generateCertificate.ts
@@ -1,22 +1,53 @@
 import PdfPrinter from "pdfmake";
 import type { TFontDictionary, TDocumentDefinitions } from "pdfmake/interfaces";
 import blobStream, { type IBlobStream } from "blob-stream";
-import logo  from "$lib/images/blue_myne_logo.png"
-const fonts: TFontDictionary = {
-  NotoSans: {
-    normal: "src/lib/server/pdf/fonts/NotoSans-Medium.ttf",
-    bold: "src/lib/server/pdf/fonts/NotoSans-Bold.ttf",
-  },
-};
-const printer = new PdfPrinter(fonts);
+import logo from "$lib/images/blue_myne_logo.png";
+import { promises as fs } from "fs";
+import fetch from "node-fetch";
+
+async function loadFont(url: string): Promise<Buffer> {
+    const response = await fetch(import.meta.env.VITE_PUBLIC_URL + url);
+    const arrayBuffer = await response.arrayBuffer();
+    return Buffer.from(arrayBuffer);
+  }
+
 async function blobToBase64(blob: Blob) {
   const buffer = Buffer.from(await blob.arrayBuffer());
   return `data:${blob.type};base64,${buffer.toString("base64")}`;
 }
+const fonts: TFontDictionary = {
+  NotoSans: {
+    normal: await loadFont("/fonts/NotoSans-Medium.ttf"),
+    bold: await loadFont("/fonts/NotoSans-Bold.ttf"),
+  },
+};
+const printer = new PdfPrinter(fonts);
 
-async function generateCertificate( cardId: string): Promise<Blob> {
+async function generateCertificate(cardId: string): Promise<Blob> {
   const dd: TDocumentDefinitions = {
-    content: [{ text: "Testing the PDFMake" }, { text: cardId }],
+    content: [{ text: "Myne Item History and Details", style: "h1" }, { text: 'Card ID:' +  cardId }],
+    styles: {
+      h1: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 16],
+      },
+      h2: {
+        fontSize: 16,
+        margin: [0, 10, 0, 10],
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 13,
+        color: "black",
+        fillColor: "#e7e7e7",
+        margin: [0, 2, 0, 2],
+      },
+      link: {
+        decoration: "underline",
+        color: "blue",
+      },
+    },
     defaultStyle: {
       font: "NotoSans",
     },
