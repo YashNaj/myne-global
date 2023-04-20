8a20db2a2ec476b54b9a342db5408f37bdcf170d
YashNaj <yashalnajeeb@gmail.com>
2023-04-19 17:10:18 -0700

    more changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import CardStatic from "$lib/components/CardStatic.svelte";
   import CardStaticStolen from "$lib/components/CardStaticStolen.svelte";
-  import MyneVideos from "$lib/components/MyneVideos.svelte";
   import homeWave from "$lib/images/homeWave.svg";
+  import MyneVideos from "$lib/components/MyneVideos.svelte";
+  import registerWave from "$lib/images/registerWave.svg";
+  import stolenWave from "$lib/images/stolenWave.svg";
   import homeWaveDesktop from "$lib/images/homeWaveDesktop.svg";
   import { inview } from "svelte-inview";
   import type { ObserverEventDetails, ScrollDirection, Options } from "svelte-inview";
@@ -32,20 +34,27 @@
     <div class="w-full h-full relative">
       <h1 class="text-black xl:text-7x md:text-6xl text-3xl font-bold">
         Think <span class="text-primary"> Safety. </span><br />
-        Your valuables 
-        <br/>
-        <span class="text-primary"> secured. </span>      </h1>
+        Your valuables
+        <br />
+        <span class="text-primary"> secured. </span>
+      </h1>
     </div>
   </div>
   <div
     class="w-full md:h-[95vh] h-[70vh] bg-[rgb(243,250,255)] relative xl:px-16 md:py-6 md:px-10 p-3 overflow-hidden flex flex-col md:flex-row"
   >
+    <div class="w-full h-full rotate-80 absolute top-0 left-10 scale-[130%] z-1 md:hidden">
+      <img src={registerWave} alt="homeWave" class="w-full h-full object-cover" />
+    </div>
+    <div class="w-full h-full rotate-80 absolute top-0 left-0 z-1 md:block hidden">
+      <img src={homeWaveDesktop} alt="homeWave" class="w-full h-full object-cover" />
+    </div>
     <h1 class="text-black xl:text-7x md:text-6xl text-3xl font-bold relative z-[2]">
       If it matters <br />
       <span class="text-primary"> Register it. </span>
     </h1>
     <div class="h-full w-full">
-      <h1 class="absolute bottom-10 w-[200px] font-semibold">
+      <h1 class="absolute bottom-10 w-[200px] font-semibold text-white">
         A robust list of categories and attributes, ever expanding and linked to your favorite merchants.
       </h1>
       <div class="container relative flex justify-center">
@@ -154,10 +163,16 @@
     </div>
   </div>
   <div class="w-full md:h-[95vh] h-[60vh] bg-[rgb(243,250,255)] relative xl:px-16 md:py-6 md:px-10 p-3">
+    <div class="w-full h-full rotate-80 absolute top-0 left-0 z-1 md:hidden">
+      <img src={stolenWave} alt="homeWave" class="w-full h-full object-cover" />
+    </div>
+    <div class="w-full h-full rotate-80 absolute top-0 left-0 z-1 md:block hidden">
+      <img src={homeWaveDesktop} alt="homeWave" class="w-full h-full object-cover" />
+    </div>
     <h1 class="text-black xl:text-7x md:text-6xl text-3xl font-bold">
       Buy With <span class="text-primary"> Confidence </span> <br />
     </h1>
-    <h1 class="font-s">
+    <h1 class="font-semibold">
       Myne's database will ping items reported stolen as such, no more guessing when purchasing second hand high value
       goods.
     </h1>
