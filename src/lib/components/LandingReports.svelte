b1879a5004fadb7edce5ea180dcc6ac1a3159b84
YashNaj <yashalnajeeb@gmail.com>
2023-04-20 18:01:12 -0700

    almost there

--- src/lib/components/LandingReports.svelte
+++ src/lib/components/LandingReports.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import reportWave from "$lib/images/reportWave.svg";
   import desktopRegister from "$lib/images/desktopRegister.svg";
+  import samplePdf from "$lib/images/samplePdf.png";
 </script>
 
-<div class="w-full md:h-[95vh] h-[70vh] bg-[rgb(243,250,255)] relative xl:px-16 md:py-6 md:px-10 p-3">
+<div class="w-full md:h-[95vh] h-[70vh] bg-[rgb(243,250,255)] relative xl:px-16 md:py-6 md:px-10 p-3 overflow-hidden">
   <div class="w-full h-full overflow-y-hidden absolute top-3 left-10 scale-[130%] z-[0] md:hidden">
     <img src={reportWave} alt="reposrtWave" class="w-full h-full object-cover" />
   </div>
@@ -16,4 +17,7 @@
   <h1 class = 'font-semibold text-primray relative'>
     Purchase reports to track repair history, sales, owners and more. Generate certificates for your own items. Never make an uniformed purchase again. 
   </h1>
+  <div class = 'w-fit h-80 overflow-hidden absolute z-[2] top-80 left-10'>
+    <img  src={samplePdf}/> 
+  </div>
 </div>
