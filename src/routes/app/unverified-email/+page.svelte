2a9e6a148f6193862e46791804720889fcb801e8
YashNaj <yashalnajeeb@gmail.com>
2023-04-26 19:02:57 -0700

    Email verify hero

--- src/routes/app/unverified-email/+page.svelte
+++ src/routes/app/unverified-email/+page.svelte
@@ -3,6 +3,13 @@
 
 
 </script>
-<div>
-    <h2 class="text-xl">Please verify your email using the link we sent you</h2>
+
+<div class="hero min-h-screen bg-white">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold text-primary">Thanks for signing up!</h1>
+      <p class="py-6 text-primary">Please verify your email using the link we sent you before you start using myne</p>
+      <button disabled class="btn btn-primary normal-case">Send another verification email</button>
+    </div>
+  </div>
 </div>
